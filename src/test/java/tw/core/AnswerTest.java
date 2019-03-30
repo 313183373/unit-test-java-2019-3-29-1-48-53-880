14b97072b2058672ec58045beb96dd842817250a
xion <313183373@qq.com>
2019-03-30 21:42:28 +0800

    add Answer.create test

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,8 +1,21 @@
 package tw.core;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
  */
 public class AnswerTest {
 
+    @Test
+    public void should_return_a_answer_when_create_a_answer_with_string() {
+        //given
+        String answerString = "1 2 3 4";
+        Answer answer = Answer.createAnswer(answerString);
+        //when
+        //then
+        assertEquals(answerString, answer.toString());
+    }
 }
