677610153132382f48f5f8f88e7c98ce8384a549
xion <313183373@qq.com>
2019-03-30 21:33:56 +0800

    add a test to check if it is work

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,34 @@
 package tw.core.generator;
 
+import org.junit.Test;
+import tw.core.Answer;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
+
+    @Test
+    public void should_return_a_standard_answer() {
+        //given
+        RandomIntGenerator randomIntGenerator = mock(RandomIntGenerator.class);
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+        String expected = "1 2 3 4";
+        //when
+        when(randomIntGenerator.generateNums(9, 4)).thenReturn(expected);
+        //then
+        try {
+            Answer answer = answerGenerator.generate();
+            assertEquals(expected, answer.toString());
+        } catch (OutOfRangeAnswerException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
 
