7d21e2d762f1e41f08a31e020a7a25b95dfaf388
xion <313183373@qq.com>
2019-03-30 23:06:20 +0800

    add guess test to check whether the guessResult is right

--- src/test/java/tw/core/GameTest.java
+++ src/test/java/tw/core/GameTest.java
@@ -1,5 +1,14 @@
 package tw.core;
 
+import org.junit.Test;
+import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.generator.AnswerGenerator;
+import tw.core.model.GuessResult;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * 在GameTest文件中完成Game中对应的单元测试
  */
@@ -7,4 +16,18 @@ package tw.core;
 
 public class GameTest {
 
+    @Test
+    public void should_return_a_answer_result_after_input_a_answer() {
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        String answerString = "1 2 3 4";
+        String guessString = "3 5 4 1";
+        try {
+            when(answerGenerator.generate()).thenReturn(Answer.createAnswer(answerString));
+            Game game = new Game(answerGenerator);
+            GuessResult guessResult = game.guess(Answer.createAnswer(guessString));
+            assertEquals("0A3B", guessResult.getResult());
+        } catch (OutOfRangeAnswerException e) {
+            e.printStackTrace();
+        }
+    }
 }
