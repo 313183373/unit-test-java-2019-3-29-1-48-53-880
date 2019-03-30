e4a98214a92a206713e928e0050a237692d1c5aa
xion <313183373@qq.com>
2019-03-31 00:37:40 +0800

    add guess history test to check whether history is stored correctly

--- src/test/java/tw/core/GameTest.java
+++ src/test/java/tw/core/GameTest.java
@@ -5,6 +5,9 @@ import tw.core.exception.OutOfRangeAnswerException;
 import tw.core.generator.AnswerGenerator;
 import tw.core.model.GuessResult;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -20,12 +23,32 @@ public class GameTest {
     public void should_return_a_answer_result_after_input_a_answer() {
         AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
         String answerString = "1 2 3 4";
-        String guessString = "3 5 4 1";
+        String guessString = "1 5 4 3";
         try {
             when(answerGenerator.generate()).thenReturn(Answer.createAnswer(answerString));
             Game game = new Game(answerGenerator);
             GuessResult guessResult = game.guess(Answer.createAnswer(guessString));
-            assertEquals("0A3B", guessResult.getResult());
+            assertEquals("1A2B", guessResult.getResult());
+        } catch (OutOfRangeAnswerException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void should_return_a_guess_history_after_input_some_answer() {
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        String answerString = "1 2 3 4";
+        String guessStringOne = "1 5 4 3";
+        String guessStringTwo = "1 2 4 3";
+        List<GuessResult> guessHistory = new ArrayList<GuessResult>();
+        try {
+            when(answerGenerator.generate()).thenReturn(Answer.createAnswer(answerString));
+            Game game = new Game(answerGenerator);
+            GuessResult guessResultOne = game.guess(Answer.createAnswer(guessStringOne));
+            GuessResult guessResultTwo = game.guess(Answer.createAnswer(guessStringTwo));
+            guessHistory.add(guessResultOne);
+            guessHistory.add(guessResultTwo);
+            assertEquals(guessHistory, game.guessHistory());
         } catch (OutOfRangeAnswerException e) {
             e.printStackTrace();
         }
