50e561908d4316f9f52de3c6cc4f72777482bad9
xion <313183373@qq.com>
2019-03-31 10:17:32 +0800

    add guess status test

--- src/test/java/tw/core/GameTest.java
+++ src/test/java/tw/core/GameTest.java
@@ -8,7 +8,7 @@ import tw.core.model.GuessResult;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -33,14 +33,14 @@ public class GameTest {
             e.printStackTrace();
         }
     }
-    
+
     @Test
     public void should_return_a_guess_history_after_input_some_answer() {
         AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
         String answerString = "1 2 3 4";
         String guessStringOne = "1 5 4 3";
         String guessStringTwo = "1 2 4 3";
-        List<GuessResult> guessHistory = new ArrayList<GuessResult>();
+        List<GuessResult> guessHistory = new ArrayList<>();
         try {
             when(answerGenerator.generate()).thenReturn(Answer.createAnswer(answerString));
             Game game = new Game(answerGenerator);
@@ -49,6 +49,64 @@ public class GameTest {
             guessHistory.add(guessResultOne);
             guessHistory.add(guessResultTwo);
             assertEquals(guessHistory, game.guessHistory());
+        } catch (OutOfRangeAnswerException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void should_return_true_status_when_game_is_not_over() {
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        String answerString = "1 2 3 4";
+        String guessStringOne = "1 5 4 3";
+        String guessStringTwo = "1 2 4 3";
+        try {
+            when(answerGenerator.generate()).thenReturn(Answer.createAnswer(answerString));
+            Game game = new Game(answerGenerator);
+            game.guess(Answer.createAnswer(guessStringOne));
+            game.guess(Answer.createAnswer(guessStringTwo));
+            assertTrue(game.checkCoutinue());
+        } catch (OutOfRangeAnswerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void should_return_false_status_when_guess_time_exceed() {
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        String answerString = "1 2 3 4";
+        String guessStringOne = "1 5 4 3";
+        String guessStringTwo = "1 2 4 3";
+        try {
+            when(answerGenerator.generate()).thenReturn(Answer.createAnswer(answerString));
+            Game game = new Game(answerGenerator);
+            game.guess(Answer.createAnswer(guessStringOne));
+            game.guess(Answer.createAnswer(guessStringTwo));
+            game.guess(Answer.createAnswer(guessStringTwo));
+            game.guess(Answer.createAnswer(guessStringTwo));
+            game.guess(Answer.createAnswer(guessStringTwo));
+            game.guess(Answer.createAnswer(guessStringTwo));
+            assertFalse(game.checkCoutinue());
+        } catch (OutOfRangeAnswerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void should_return_false_status_when_guess_right() {
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        String answerString = "1 2 3 4";
+        String guessStringOne = "1 5 4 3";
+        String guessStringTwo = "1 2 4 3";
+        String guessStringThree = "1 2 3 4";
+        try {
+            when(answerGenerator.generate()).thenReturn(Answer.createAnswer(answerString));
+            Game game = new Game(answerGenerator);
+            game.guess(Answer.createAnswer(guessStringOne));
+            game.guess(Answer.createAnswer(guessStringTwo));
+            game.guess(Answer.createAnswer(guessStringThree));
+            assertFalse(game.checkCoutinue());
         } catch (OutOfRangeAnswerException e) {
             e.printStackTrace();
         }
